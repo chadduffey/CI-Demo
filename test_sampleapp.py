f6627f0f067843873cc85ddfe2d14ed7734d75ea
Chad Duffey <7192541+chadduffey@users.noreply.github.com>
2020-01-15 18:00:03 -0800

    fixed pep issues

--- test_sampleapp.py
+++ test_sampleapp.py
@@ -7,10 +7,8 @@ import sampleapp
 
 class TestSampleApp:
 
-	def test_addition(self):
-		assert 4 == sampleapp.add(2,2)
+    def test_addition(self):
+        assert 4 == sampleapp.add(2, 2)
 
-
-	def test_subtraction(self):
-		assert 2 == sampleapp.subtract(4,2)
-
+    def test_subtraction(self):
+        assert 2 == sampleapp.subtract(4, 2)
